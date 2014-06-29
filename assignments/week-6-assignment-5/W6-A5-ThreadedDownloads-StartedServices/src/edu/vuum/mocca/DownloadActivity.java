5b153ebf6f4707018eafb03059b225802330ecb2
Rafael Fernandes de Oliveira <rafaelolivei@gmail.com>
2014-06-29 15:35:26 -0300

    Conclusão da semana 6
    Falta testar.

--- assignments/week-6-assignment-5/W6-A5-ThreadedDownloads-StartedServices/src/edu/vuum/mocca/DownloadActivity.java
+++ assignments/week-6-assignment-5/W6-A5-ThreadedDownloads-StartedServices/src/edu/vuum/mocca/DownloadActivity.java
@@ -78,6 +78,7 @@ public class DownloadActivity extends DownloadBase {
                 // bitmap that's been downloaded and returned to
                 // the DownloadActivity as a pathname who's Bundle
             	// key is defined by DownloadUtils.PATHNAME_KEY
+            	activity.displayBitmap(DownloadUtils.PATHNAME_KEY);
             }
     	}
     }
@@ -107,6 +108,7 @@ public class DownloadActivity extends DownloadBase {
             // TODO - You fill in here to start the
             // DownloadIntentService with the appropriate Intent
             // returned from the makeIntent() factory method.
+        	DownloadIntentService.makeIntent(view.getContext(), handler, getUrlString());
 
             which = "Starting DownloadIntentService";
             break;
@@ -115,6 +117,7 @@ public class DownloadActivity extends DownloadBase {
             // TODO - You fill in here to start the
             // ThreadPoolDownloadService with the appropriate Intent
             // returned from the makeIntent() factory method.
+        	ThreadPoolDownloadService.makeIntent(getApplicationContext(), handler, getUrlString());
 
             which = "Starting ThreadPoolDownloadService";
             break;
